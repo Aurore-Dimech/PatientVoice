77cac0b78d2e093286e1da2a894fdd03cb7259c0
Pierric Letard <pierric@radiomail-fr.com>
2025-12-12 14:03:41 +0100

    ajout page Form

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Search from "../views/Search.vue";
 import Accueil from "../views/Accueil.vue";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: "/",
-			name: "App",
+			name: "Accueil",
 			component: Accueil,
+		},
+		{
+			path: "/search",
+			name: "Search",
+			component: Search,
 		},
 	],
 });
