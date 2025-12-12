e4631e7e8c3a59e32ca14ca23d18aa490da996a0
Pierric Letard <pierric@radiomail-fr.com>
2025-12-12 14:08:21 +0100

    rename search in centers

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Search from "../views/Search.vue";
+import Search from "../views/Centers.vue";
 import Accueil from "../views/Accueil.vue";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +10,8 @@ const router = createRouter({
 			component: Accueil,
 		},
 		{
-			path: "/search",
-			name: "Search",
+			path: "/centers",
+			name: "Centers",
 			component: Search,
 		},
 	],
