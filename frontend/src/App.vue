6ded7c9d332f9054ef8fb940c75a6cdbeb232428
LauraKoNk <l.kokonyange@gmail.com>
2026-01-07 13:59:59 +0100

    ajout header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import Header from './views/Header.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen">
+    <Header />
+    <div class="flex-1">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
---
+++ frontend/src/views/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="bg-gray-200 flex items-center justify-between px-12">
+      <div class="flex items-center space-x-4">
+        <img src="@/assets/PatientVoice-logo.png" alt="PatientVoice" class="h-40 w-auto" />
+      </div>
+      <nav class="flex space-x-12">
+        <a href="/" class="text-lg text-gray-900 hover:text-purple-800 font-medium">Home</a>
+        <a href="/centers" class="text-lg text-gray-900 hover:text-purple-800 font-medium">Les centres</a>
+        <a href="/contact" class="text-lg text-gray-900 hover:text-purple-800 font-medium">Contact</a>
+      </nav>
+    </header>
+</template>
+<script setup>
+    
+</script>
