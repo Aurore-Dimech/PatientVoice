acb16614ed66194c632c25efb10a3c357b6d1ccc
Aurore Dimech <aurore.dimech@edu.devinci.fr>
2026-01-28 23:38:54 +0100

    fix: config for sequelize

---
+++ backend/config/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();  // Load env vars
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+};
